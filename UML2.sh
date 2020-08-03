7e1d479c937af97bea1ba395b8945e2fced9ae13
Plantkisser <44616622+Plantkisser@users.noreply.github.com>
2020-08-04 02:01:39 +0500

    Add files via upload

--- UML2.sh
+++ UML2.sh
@@ -1,6 +1,14 @@
+#!/bin/sh
+
+git clone https://github.com/sleirsgoevy/heroku-buildpack-uml.git
+cd heroku-buildpack-uml
+tar xzf uml.tar.gz
+mv .uml/slirp ~/UML/
+cd ../
+sudo rm -R heroku-buildpack-uml
 mkdir ~/UML/FS
 sudo dnf install podman-docker
-docker run -it --name test centos:8.1.1911
+docker create --name test centos:8.1.1911
 docker cp test:/bin ~/UML/FS/
 docker cp test:/dev ~/UML/FS/
 docker cp test:/etc ~/UML/FS/
@@ -23,11 +31,10 @@ docker cp test:/var ~/UML/FS/
 wget https://www.busybox.net/downloads/binaries/1.31.0-defconfig-multiarch-musl/busybox-x86_64
 mv busybox-x86_64 ~/UML/FS/bin/
 cd ~/UML/FS/bin/
+rm sh
 ln -n busybox-x86_64 sh
 ln -n busybox-x86_64 ifconfig
 ln -n busybox-x86_64 route
 chown -R $USER ~/UML/FS
 chmod -R 0775 ~/UML/FS
-cd ~/UML/linux-4.18.0-147.8.1.el8_1
-
 
